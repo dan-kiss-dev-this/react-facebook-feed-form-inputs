cee96bd08d0aa536e338e5934fed3194ad5907b7
Dan Kiss <flowfast47@gmail.com>
2019-03-13 14:17:32 -0700

    add way to collect title and image url into an object

--- src/App.js
+++ src/App.js
@@ -2,66 +2,76 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const post = (dataObject, Index) => {
-  return (
-    <p key={Index}>{dataObject.title}</p>
-  );
-};
+const post = (dataObject, Index) => <p key={Index}>{dataObject}</p>;
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = { 
-      value: "",
+      valueTitle: "",
+      valueImage: "",
       allValues: [],
     };
 
-    this.handleChange = this.handleChange.bind(this);
+    this.handleChangeTitle = this.handleChangeTitle.bind(this);
+    this.handleChangeImage = this.handleChangeImage.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleChange(event) {
-    this.setState({ value: event.target.value });
+  handleChangeTitle(event) {
+    this.setState({ valueTitle: event.target.value });
+  }
+
+  handleChangeImage(event) {
+    this.setState({ valueImage: event.target.value });
   }
   
   async handleSubmit(event) {
     // alert('A value was submitted: ' + this.state.value);
     event.preventDefault();
-    await this.setState({ allValues: [...this.state.allValues, this.state.value] });
-    this.setState({ value: ""});
+    const postObject = { title: this.state.valueTitle, imageURL: this.state.valueImage };
+    // const postObject = new Object(this.state.valueTitle, this.state.valueImage,)
+    console.log(postObject);
+    await this.setState({ allValues: [...this.state.allValues, this.state.valueTitle] });
+    this.setState({ valueTitle: ""});
     console.log(this.state.allValues);
   }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        <div className="App-header">
 
-        <form onSubmit={this.handleSubmit}>
-          <label>
-            Title of Image:
-          <input type="text" value={this.state.value} onChange={this.handleChange} />
-          <input type="submit" value="Submit Post" />
-          </label>
-        </form>
+          <form onSubmit={this.handleSubmit}>
+            <label>
+              Title of Image:
+            <input type="text" value={this.state.valueTitle} onChange={this.handleChangeTitle} />
+            </label>
+            <br/>
+            <label>
+              URL Link to Image:
+            <input type="text" value={this.state.valueImage} onChange={this.handleChangeImage} />
+            <input type="submit" value="Submit Post" />
+            </label>
+          </form>
 
-        <div>
-          {this.state.allValues.map((singlePost, index) => post(singlePost, index))}
-        </div>
+          <div>
+            {this.state.allValues.map((singlePost, index) => post(singlePost, index))}
+          </div>
 
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </div>  
       </div>
     );
   }
