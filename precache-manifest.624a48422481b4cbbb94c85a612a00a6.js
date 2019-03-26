8edb5825560698df46a70d5fe6c2f350049b1811
Dan Kiss <flowfast47@gmail.com>
2019-03-26 15:05:24 -0700

    Updates

---
+++ precache-manifest.624a48422481b4cbbb94c85a612a00a6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d9188ea5077120af1249",
+    "url": "/react-facebook-feed-form-inputs/static/js/runtime~main.2cc89087.js"
+  },
+  {
+    "revision": "2f977f037a455c071024",
+    "url": "/react-facebook-feed-form-inputs/static/js/main.314419fe.chunk.js"
+  },
+  {
+    "revision": "4827334cdc504dc2e274",
+    "url": "/react-facebook-feed-form-inputs/static/js/2.5410efac.chunk.js"
+  },
+  {
+    "revision": "2f977f037a455c071024",
+    "url": "/react-facebook-feed-form-inputs/static/css/main.0c9f267a.chunk.css"
+  },
+  {
+    "revision": "bb7d50d0eadc1e6a7d63d2c00d0a1a4e",
+    "url": "/react-facebook-feed-form-inputs/index.html"
+  }
+];
